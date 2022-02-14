b496a862ec4c27b7283b68144dd1551f00bb7b6d
FranciscoPerego <francisco.perego@gmail.com>
2022-02-14 20:21:50 -0300

    Estilos nuevos

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,13 +1,13 @@
 
 
 
-export const ItemDetail = ( {nombre, id , precio, img , stock , categoria}) =>{
+export const ItemDetail = ( {nombre, id , precio, img , stock , categoria, descripcion}) =>{
   return (
-    <div>
+    <div className="centrar">
         <h3 className="margenSuperior3">{nombre}</h3>
         <img src={img} alt={nombre}/>
-        <p>{categoria}</p>
-        <h4>Precio:US${precio}</h4>
+        <p className="margenSuperior4">{descripcion}</p>
+        <h4 className="letrasRojas">Precio:US${precio}</h4>
     </div>
   )
 }
